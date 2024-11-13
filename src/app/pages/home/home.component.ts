841bd26fffc72c84dbd15f926fb3d30368ae510e
amirhilal044 <amiramirhilal@gmail.com>
2024-11-13 17:57:32 +0200

    added filteredProducts to home page to search for products

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ProductsService } from '../../services/api/products.service';
 import { AuthService } from '../../services/auth/auth.service';
@@ -7,13 +8,19 @@ import { AuthService } from '../../services/auth/auth.service';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
   products = signal<any[]>([]);
   loading = signal<boolean>(false);
+  searchTerm = signal<string>('');
+  filteredProducts = computed(() =>
+    this.products().filter((product) =>
+      product.title.toLowerCase().includes(this.searchTerm().toLowerCase())
+    )
+  );
   categories: string[] = [
     'Electronics',
     'Jewelry',
