c447a53ceaebe9a25b713798fbf59b9f843e602f
amirhilal044 <amiramirhilal@gmail.com>
2024-11-13 18:21:46 +0200

    added animation into product details

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,7 +1,8 @@
+import { animate, style, transition, trigger } from '@angular/animations';
 import { CommonModule } from '@angular/common';
-import { Component, signal, computed } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, computed, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { ProductsService } from '../../services/api/products.service';
 import { AuthService } from '../../services/auth/auth.service';
 
@@ -11,6 +12,19 @@ import { AuthService } from '../../services/auth/auth.service';
   imports: [CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms ease-in', style({ opacity: 1 })),
+      ]),
+      transition(':leave', [animate('300ms ease-out', style({ opacity: 0 }))]),
+      transition('* => *', [
+        style({ opacity: 0 }),
+        animate('400ms ease-in-out', style({ opacity: 1 })),
+      ]),
+    ]),
+  ],
 })
 export class HomeComponent {
   products = signal<any[]>([]);
@@ -21,14 +35,16 @@ export class HomeComponent {
   );
   loading = signal<boolean>(false);
   searchTerm = signal<string>('');
-  selectedProduct = signal<any | null>(null); 
+  selectedProduct = signal<any | null>(null);
+  recommendedProduct = signal<any | null>(null);
+  animationState = signal<boolean>(true);
+
   categories: string[] = [
     'Electronics',
     'Jewelry',
     'Men Clothing',
     'Women Clothing',
   ];
-  title = 'Welcome to E-Comz';
 
   constructor(
     private authService: AuthService,
@@ -44,13 +60,25 @@ export class HomeComponent {
     this.loading.set(true);
 
     this.productsService.getProducts().subscribe((data: any) => {
-      this.products.update((prevProducts) => [...prevProducts, ...data]);
+      this.products.set(data);
+      this.selectRandomRecommendedProduct();
       this.loading.set(false);
     });
   }
 
+  selectRandomRecommendedProduct() {
+    const productsList = this.products();
+    if (productsList.length > 0) {
+      const randomIndex = Math.floor(Math.random() * productsList.length);
+      this.recommendedProduct.set(productsList[randomIndex]);
+      this.selectedProduct.set(this.recommendedProduct());
+    }
+  }
+
   selectProduct(product: any) {
+    this.animationState.set(!this.animationState()); // Toggle the animation state to trigger reanimation
     this.selectedProduct.set(product);
+    this.recommendedProduct.set(null); // Clear the recommended product
   }
 
   onLogout() {
