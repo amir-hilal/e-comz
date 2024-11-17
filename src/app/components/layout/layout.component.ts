635345d30ce464ea5a7b36a73f6b030e494ec941
amirhilal044 <amiramirhilal@gmail.com>
2024-11-17 15:19:01 +0200

    created footer component

--- src/app/components/layout/layout.component.ts
+++ src/app/components/layout/layout.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '../../footer/footer.component';
 import { NavbarComponent } from '../navbar/navbar.component';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, RouterOutlet],
+  imports: [CommonModule, NavbarComponent, RouterOutlet, FooterComponent],
   templateUrl: './layout.component.html',
   styleUrls: ['./layout.component.scss'],
 })
