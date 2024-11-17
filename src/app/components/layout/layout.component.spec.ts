a090919be7f7b19760eaeccf512878ccffa3deb0
amirhilal044 <amiramirhilal@gmail.com>
2024-11-17 16:14:09 +0200

    created layout component unit test

--- src/app/components/layout/layout.component.spec.ts
+++ src/app/components/layout/layout.component.spec.ts
@@ -1,6 +1,24 @@
+import { InjectionToken } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { provideRouter } from '@angular/router';
+import { of } from 'rxjs';
 import { LayoutComponent } from './layout.component';
+
+class MockAngularFireAuth {
+  authState = of(null);
+}
+
+class MockAngularFirestore {
+  collection = jasmine.createSpy('collection').and.returnValue({
+    doc: jasmine.createSpy('doc').and.returnValue({
+      set: jasmine.createSpy('set').and.returnValue(Promise.resolve()),
+    }),
+  });
+}
+
+const mockFirebaseConfig = {};
 
 describe('LayoutComponent', () => {
   let component: LayoutComponent;
@@ -8,9 +26,17 @@ describe('LayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [LayoutComponent]
-    })
-    .compileComponents();
+      imports: [LayoutComponent],
+      providers: [
+        { provide: AngularFireAuth, useClass: MockAngularFireAuth },
+        { provide: AngularFirestore, useClass: MockAngularFirestore },
+        {
+          provide: new InjectionToken('angularfire2.app.options'),
+          useValue: mockFirebaseConfig,
+        },
+        provideRouter([]), 
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(LayoutComponent);
     component = fixture.componentInstance;
